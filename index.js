14b763239f4a7181baed74cb412b8070d64d9876
b09902105 <b09902105@linux9.csie.ntu.edu.tw>
2022-11-25 17:45:46 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+//reference: https://reurl.cc/917QLn
+var http = require('http'); //load http module
+const hostname = 'linux9.csie.ntu.edu.tw';
+const port = 9025;
+const server = http.createServer((req, res) => { //function(req, res)
+    res.statusCode = 200; // OK
+    res.setHeader = ('content-Type', 'text/html');
+    res.write(`<h1>Test server</h1>`);
+    res.end();
+});
+server.listen(port, hostname, () => {
+    console.log(`The server is listening on http://${hostname}:${port}`);
+});
